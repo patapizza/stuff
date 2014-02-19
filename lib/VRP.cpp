ad2fd0f9171b9487bd4f1c71c692f4137aaef3b0
Michael Saint-Guillain <stguillain@mac-1m0-356.local>
2014-02-19 12:13:49 +0100

    organizational stuff

---
+++ lib/VRP.cpp
@@ -0,0 +1,230 @@
+#include <iostream>
+#include <fstream>	// file I/O
+#include <cstdlib>	// rand
+#include <ctime>	// time
+#include <cmath>	// sqrt
+#include <cstring>	// memcpy
+#include <limits>   // numeric_limits
+
+#include "VRPlib.h"
+
+using namespace std;
+
+
+/* Problem data variables */
+int N;		// number of vertices [1..N]
+int NVeh;	// number of vehicles [1..NVeh]
+int Q;		// vehicle max capacity
+double **c;
+float *coordX; float *coordY;
+int *demand;
+
+
+/* Handle instance files of Cordeau-Laporte vrp/old */
+void readInstanceFileCordeauLaporteVRPold(const char *filename) {
+	int i;
+	std::string line;
+	std::ifstream instance_file;
+	instance_file.open(filename);
+	if (instance_file.fail()) {
+		std::cerr << "Unable to open " << filename;
+		exit (8);
+	}
+
+	instance_file >> i; 	// skip first integer
+	instance_file >> NVeh;	// retrieve number of vehicles
+	instance_file >> N;		// retrieve instance size
+	c = new double*[NVeh+N+1];
+	for(int i=0; i<NVeh+N+1; i++) c[i] = new double[NVeh+N+1];
+
+	coordX = new float[NVeh+N+1]; 
+	coordY = new float[NVeh+N+1];
+	demand = new int[NVeh+N+1];
+	std::getline(instance_file, line); // skip end of line
+
+	instance_file >> i;			// skip int
+	instance_file >> Q;			// retrieve max capacity
+
+	instance_file >> coordX[1]; 		// retrieve x coordinate
+	instance_file >> coordY[1]; 		// retrieve y coordinate
+	std::getline(instance_file, line);	// skip end of line
+	for (int r=2; r<NVeh+1; r++) {
+		coordX[r] = coordX[1];
+		coordY[r] = coordY[1];
+	} 
+	for (int j=1; j<N+1; j++) {
+		instance_file >> i; 				// skip int
+		instance_file >> coordX[j+NVeh]; 	// retrieve x coordinate
+		instance_file >> coordY[j+NVeh]; 	// retrieve y coordinate
+		instance_file >> i;					// skip int
+		instance_file >> demand[j+NVeh];	// retrieve demand
+		std::getline(instance_file, line);	// skip end of line
+	}
+
+	instance_file.close();
+
+	for (int i=1; i<NVeh+N+1; i++)
+		for (int j=1; j<NVeh+N+1; j++)
+			c[i][j] = std::sqrt(std::pow((coordX[i] - coordX[j]),2) + std::pow((coordY[i] - coordY[j]),2)); // compute Euclidean distances
+}
+
+/*
+inline std::ostream &operator << (std::ostream &out_file, solutionTSP& s) {
+	if (s.getViolations() > 0) out_file << "Infeasible ! ";
+	out_file << "Cost=" << s.getCost() << " ";
+	for(int i=1; i<N+1; i++)
+		out_file << s.step[i] << " ";
+	return (out_file);
+}*/
+
+
+/* class solutionVRP ********************************************************************************************
+*	defines a solution for a TSP 
+*/
+solutionVRP::solutionVRP() {											// constructor ***
+	previous = new int[NVeh+N+1];	// vertices from 1 to NVeh + N
+	next = new int[NVeh+N+1];
+	vehicle = new int[NVeh+N+1];	// vertices from 1 to Nveh + Nveh 	// vehicle[i] = 0 iff i is a depot
+
+	cout << "Generating initial solution ..." << flush;
+	generateInitialSolution();
+	cout << "Done.\n" << flush; 
+}
+solutionVRP::solutionVRP(const solutionVRP& old_solution) {				// copy constructor ***
+	previous = new int[NVeh+N+1];
+	next = new int[NVeh+N+1];
+	vehicle = new int[NVeh+N+1];
+	*this = old_solution;
+}
+solutionVRP& solutionVRP::operator = (const solutionVRP& sol) {
+	memcpy(previous, sol.previous, (NVeh+N+1)*sizeof(int));
+	memcpy(next, sol.next, (NVeh+N+1)*sizeof(int));
+	memcpy(vehicle, sol.vehicle, (NVeh+N+1)*sizeof(int));
+	return (*this);
+}
+solutionVRP::~solutionVRP() {										// destructor ***
+	delete [] previous;
+	delete [] next;
+	delete [] vehicle;
+}
+
+void solutionVRP::generateInitialSolution() {
+	int *last = new int [NVeh+1];
+	for(int r=1; r<NVeh+1; r++)	last[r] = r;
+	vehicle[0] = 0;
+	/* Assign random values */
+	for (int i=NVeh+1; i<NVeh+N+1; i++) {
+		int r = rand() % NVeh + 1;
+		vehicle[i] = r;
+		previous[i] = last[r];
+		last[r] = i;
+	}
+	for (int r=1; r<NVeh+1; r++) {
+		int n = r;
+		int i = last[r];
+		previous[r] = last[r];
+		while (i != r) {
+			next[i] = n;
+			n = i;
+			i=previous[i];
+		} next[r] = n;
+	}
+}
+void solutionVRP::shakeSolution() {
+	/* Move a vertex from its position to a 
+			1) random different position 
+			2) best insert 
+		at 50-50%
+	*/
+	int vertex, before_i;
+
+	vertex = rand() % N + (NVeh + 1); // generate random number in [NVeh+1..NVeh+N]
+
+	int chance = rand() % 100 + 1;
+	if (chance < 50) {
+		do {
+			before_i = rand() % (NVeh + N) + 1; // generate random number in [1..NVeh+N]
+		} while (before_i == vertex || before_i == next[vertex]);
+	} else {
+		float min_delta = numeric_limits<float>::max();
+		for (int i=1; i<NVeh+N+1; i++) {	// find the best position to re-insert it
+			float delta = 0.0;
+			if (i == vertex || i == next[vertex]) continue;
+			delta =   c[previous[i]][i] 
+					+ c[previous[i]][vertex]
+					+ c[vertex][i];
+			if (delta < min_delta) {
+				before_i = i;
+				min_delta = delta;
+			} 
+		} 
+	}
+	//cout << "moving " << vertex-NVeh << " to before " << before_i-NVeh << endl;
+	// Remove from current position
+	previous[next[vertex]] = previous[vertex];
+	next[previous[vertex]] = next[vertex];
+
+	// Insert elsewhere
+	vehicle[vertex] = vehicle[before_i];
+	next[vertex] = before_i;
+	previous[vertex] = previous[before_i];
+	previous[before_i] = vertex;
+	next[previous[vertex]] = vertex;
+	//cout << toString();
+}
+float solutionVRP::getCost() {
+	float cost = 0.0;
+	
+	for (int r=1; r<NVeh+1; r++) {
+		int i = previous[r];
+		while (i != r) {
+			cost += c[i][next[i]];
+			i=previous[i];
+		} cost += c[i][next[i]];
+	}
+
+	return (cost);
+}
+float solutionVRP::getViolations() {
+	float v = 0.0;
+	int *load = new int [NVeh+1];	// load[i] = x iff vehicle i has a cumulative load of x
+	
+	for (int r=1; r<NVeh+1; r++) {	// compute vehicle loads
+		int i = previous[r];
+		load[r] = 0;
+		while (i != r) {
+			load[r] += demand[i];
+			i=previous[i];
+		} 
+	}
+
+	for(int r=1; r<NVeh+1; r++) if (load[r] > Q) v += load[r] - Q;
+
+	return (v);
+}		
+
+string& solutionVRP::toString() {
+	static string str; str ="";
+	int *load = new int [NVeh+1];	// load[i] = x iff vehicle i has a cumulative load of x
+	
+	//for (int i=1; i<NVeh+N+1; i++)
+	//	cout << "(i:" << i-NVeh << ",v:" << vehicle[i]-NVeh << "," << previous[i]-NVeh << "," 
+	//			<< next[i]-NVeh << ",q:" << demand[i] << ",x:" << coordX[i] << ",y:" << coordY[i] << ") " << endl;
+	for (int r=1; r<NVeh+1; r++) {	// compute vehicle loads
+		int i = previous[r];
+		load[r] = 0;
+		while (i != r) {
+			load[r] += demand[i]; 
+			i=previous[i];
+		} 
+	}
+	if (getViolations() > 0) str += "Infeasible ! ";
+	str += "Cost=" + to_string(getCost()) + " \n";
+	for(int r=1; r<NVeh+1; r++) {
+		str += "Route " + to_string(r) + "(" + to_string(load[r]) + "): D ";
+		for(int i=next[r]; i!=r; i=next[i])
+			str += to_string(i-NVeh) + " ";
+		str += "D\n";
+	}
+	return (str);
+}
